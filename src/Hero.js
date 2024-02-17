909ad08185f4acaf703f4996b1c2df7089c52186
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-02-17 13:46:17 +0100

    good

--- src/Hero.js
+++ src/Hero.js
@@ -13,7 +13,7 @@ const Hero = () => {
             for the internet
           </h1>
           <p>
-            Millions of companies of all sizes—from startups to Fortune 500s—use
+            Millions of companies of all sizes—from startups to all Fortune 500s—use
             Stripe’s software and APIs to accept payments, send payouts, and
             manage their businesses online.
           </p>
